bb234043018e08220356921ec888328568c1415f
SlyFryFrog <78104059+SlyFryFrog@users.noreply.github.com>
2022-01-12 15:34:26 -0500

    Update Flash Cards Program.py

--- Flash Cards Program.py
+++ Flash Cards Program.py
@@ -1,4 +1,5 @@
 # Flash Cards Program
+from os import remove
 import tkinter
 import random
 import time
@@ -6,34 +7,38 @@ import time
 # Imports code for gui from another file
 import gui
 
-def main():
+def build_dict():
+    dictionary = {}
+
+    # Builds a dictionary from text file and sets everything to lowercase
+    with open("verbs.txt") as f:
+        for line in f:
+            (key, val) = line.split(", ")
+            dictionary[key] = val.strip("\n")
+    dictionary =  {key.lower(): val.lower() for key, val in dictionary.items()}
+    return dictionary
+
+def flashcard(dictionary):
+    print('Type "exit" to exit the program.')
+
     while True:
-        def flashCard():
-            print(f'Translate "{current[1]}"')
-            user_input = input("Answer here: ")
-            if user_input.lower() == current[0]:
-                print("Correct!")
-            else:
-                print("Please try again.\n")
-                flashCard()
 
-        dictionary = {}
-        current = ()
-        user_input = ""
+        # Generates a random set of items from dictionary
+        current_pair = random.choice(list(dictionary.items()))
 
-        # Sets dictionary keys, values as typed in verbs.txt
-        with open("verbs.txt") as f:
-            for line in f:
-                (key, val) = line.split(", ")
-                dictionary[key] = val.strip("\n")
+        print(f'Translate "{current_pair[1]}"')
 
-        # Converts all text in dictionary to lowercase
-        # and sets current to a random key and its matching value
-        dictionary =  {key.lower(): val.lower() for key, val in dictionary.items()}
-        current = random.choice(list(dictionary.items()))
-        print(dictionary)
-        
+        user_input = input("Answer here: ")
+        if user_input.lower() == current_pair[0]:
+            print("Correct!\n")
+        elif user_input.lower() == "exit":
+            break
+        else:
+            print("Please try again.\n")
 
-        print(current)
-        flashCard()
+
+def main():
+    dictionary = build_dict()
+    flashcard(dictionary)
+
 main()
